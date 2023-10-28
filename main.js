0c29161b5be041e9cf9ada64108055bc2bcd3c80
Pedro Josué <74951282+pedrojosuesalomao@users.noreply.github.com>
2023-10-27 23:34:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function calc(peso,litros){
+    var peso = Number(document.getElementById('caixa1').value)
+    var litros = 35
+    var result =  peso * litros
+    document.getElementById('mm').innerHTML =  "Você deve beber " + result + "L por dia!"
+
+}
+   //@pedrosalomao_
+   //um dos meus projetos de estudo com javascript, este é bem básico. 
